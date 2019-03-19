45584d3d50359c98c344ad9809d63a4e9e232f07
Nate Bachmeier <nate.bachmeier@renaissance.com>
2019-03-19 00:43:23 -0700

    Cpp Refresher: Add taco class and explore behavior

--- Week3_AnalyzeTemplates/CppRefresher/src/hello.cpp
+++ Week3_AnalyzeTemplates/CppRefresher/src/hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include "hello.h"
+#include "taco.h"
 using namespace std;
 
 void say_taco()
@@ -11,5 +12,12 @@ void say_taco()
 
 int main(int argc, char**argv)
 {
+    Taco taco;
+    taco.set_quantity(7);
+    taco.set_meat("beef");
+
     printf("Hello there!\n");
+    printf("Can I have %d %s tacos? Thanks!\n", 
+        taco.get_quantity(),
+        taco.get_meat().c_str());
 }
