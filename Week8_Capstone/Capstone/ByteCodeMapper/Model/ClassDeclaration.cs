59d452313ab807f8650ad7b60ff2bfec7e443ddf
Nate Bachmeier <nate.bachmeier@renaissance.com>
2019-04-22 23:06:32 -0700

    Initial code for parsing the assembly files

---
+++ Week8_Capstone/Capstone/ByteCodeMapper/Model/ClassDeclaration.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ByteCodeMapper.Model
+{
+    public class ClassDeclaration
+    {
+        public string Name { get; set; }
+
+        public string Extends { get; set; }
+
+        public List<string> Implements { get; set; } = new List<string>();
+
+        public List<MethodDeclaration> Methods { get; set; } = new List<MethodDeclaration>();
+
+        public ClassDeclaration(string declaration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClassDeclaration(StreamReader reader)
+        {
+            var line = string.Empty;
+            var previousLine = string.Empty;
+            ClassDeclaration classDefinition = null;
+            while ((line = reader.ReadLine()?.Trim()) != null)
+            {
+                var lineType = GetLineType(line);
+                if (lineType == LineType.Ignore)
+                {
+                    continue;
+                }
+
+                switch (lineType)
+                {
+                    case LineType.BeginClassDefinition:
+                        classDefinition = new ClassDeclaration(line);
+                        break;
+
+                    case LineType.BeginMethod:
+                        classDefinition.Methods.Add(new MethodDeclaration(
+                            declaration: previousLine,
+                            reader: reader));
+                        break;
+                }
+
+                previousLine = line;
+            }
+        }
+
+
+        private LineType GetLineType(string line)
+        {
+            if (line.EndsWith("{"))
+            {
+                return LineType.BeginClassDefinition;
+            }
+            else if (line.StartsWith("Code:"))
+            {
+                return LineType.BeginMethod;
+            }
+            else
+            {
+                return LineType.Ignore;
+            }
+        }
+
+        private enum LineType
+        {
+            BeginClassDefinition,
+            BeginMethod,
+            Ignore
+        }
+    }
+}
--- Week8_Capstone/Capstone/Capstone.Model/GraphML.cs
+++ Week8_Capstone/Capstone/Capstone.Model/GraphML.cs
@@ -1,50 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml;
-using System.Xml.Serialization;
 
 namespace Capstone.Model
 {
     public class GraphML
-    {
-        public class GraphNode
-        {
-            [XmlAttribute("id")]
-            public string Id { get; set; }
-
-            [XmlAttribute("edgedefault")]
-            public EdgeMode EdgeDefault { get; set; } = EdgeMode.directed;
-
-            public enum EdgeMode
-            {
-                directed,
-                undirected
-            }
-
-            public List<GraphElement> Children { get; set; } = new List<GraphElement>();
-
-            public void AppendXml(XmlElement rootNode)
-            {
-                var node = rootNode.OwnerDocument.CreateElement("graph");
-                rootNode.AppendChild(node);
-
-                var att = node.OwnerDocument.CreateAttribute("id");
-                att.Value = this.Id;
-                node.Attributes.Append(att);
-
-                att = node.OwnerDocument.CreateAttribute("edgedefault");
-                att.Value = Convert.ToString(this.EdgeDefault);
-                node.Attributes.Append(att);
-
-                foreach (var child in this.Children)
-                {
-                    child.AppendXml(node);
-                }
-            }
-        }
+    {        
         public List<MetadataKey> Keys { get; set; } = new List<MetadataKey>();
 
         public GraphNode Graph { get; set; } = new GraphNode { Id = "G" };
+
+        public Vertex GetOrCreateVertex(string id, Func<Vertex> create)
+        {
+            var existing = this.Graph.Children.OfType<Vertex>().SingleOrDefault(
+                v => string.Equals(v.Id, id, StringComparison.OrdinalIgnoreCase));
+
+            if (existing != null)
+            {
+                return existing;
+            }
+
+            var node = create();
+            this.Graph.Children.Add(node);
+            return node;
+        }
 
         public XmlDocument GetXmlDocument()
         {
